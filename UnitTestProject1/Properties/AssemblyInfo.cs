9eeb6bb090fb8b7042adb3542eebcbdbb1b80266
AlexSlz <30480995+AlexSlz@users.noreply.github.com>
2022-10-06 13:54:40 +0300

    Add lab 3 + UnitTest

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0ad1454f-2417-45a3-881d-0e7fc844979e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using blockSchemeEditor;
+using blockSchemeEditor.Elements;
+using System.Drawing;
+using blockSchemeEditor.Elements.ElementsData;
+using System.IO;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ElementObject TestElement = new ElementObject(new Point(0, 0), new MyRectangle());
+        Canvas canvas = new Canvas();
+        private ElementObject CreateElement(IElement prefab)
+        {
+            ElementObject element = new ElementObject(new Point(0, 0), prefab);
+            Assert.AreEqual(element.Parameters.Text, prefab.Name);
+            return element;
+        }
+        private void TestAddElementToCanvas(ElementObject element)
+        {
+            canvas.Elements.Add(element);
+            Assert.AreEqual(canvas.Elements.Find(item => item == element).Id, element.Id);
+        }
+        [TestMethod]
+        public void CreateMyRectangle()
+        {
+            var element = CreateElement(new MyRectangle());
+            TestAddElementToCanvas(element);
+        }
+        [TestMethod]
+        public void CreateEllipse()
+        {
+            var element = CreateElement(new Ellipse());
+            TestAddElementToCanvas(element);
+        }
+        [TestMethod]
+        public void CreateRoundedRectangle()
+        {
+            var element = CreateElement(new RoundedRectangle());
+            TestAddElementToCanvas(element);
+        }
+        [TestMethod]
+        public void CreateRhombus()
+        {
+            var element = CreateElement(new Rhombus());
+            TestAddElementToCanvas(element);
+        }
+        [TestMethod]
+        public void CreateLine()
+        {
+            var SecondElement = new ElementObject(new Point(0, 0), new MyRectangle());
+            Line line = new Line(TestElement.Nodes[0], SecondElement.Nodes[0]);
+            Assert.AreEqual(line.FirstNode.Parent, TestElement);
+            Assert.AreEqual(line.SecondNode.Parent, SecondElement);
+        }
+        [TestMethod]
+        public void TestAddLineToCanvas()
+        {
+            var SecondElement = new ElementObject(new Point(0, 0), new MyRectangle());
+            Line line = new Line(TestElement.Nodes[0], SecondElement.Nodes[0]);
+            canvas.Lines.Add(line);
+            Assert.AreEqual(canvas.Lines[0].FirstNode.Parent, TestElement);
+            Assert.AreEqual(canvas.Lines[0].SecondNode.Parent, SecondElement);
+        }
+        [TestMethod]
+        public void TestMoveElement()
+        {
+            Assert.AreEqual(TestElement.Parameters.Position, new Point(0, 0));
+            TestElement.Move(new Point(10, 10), new Point(0,0), TestElement.Parameters.Position);
+            Assert.AreEqual(TestElement.Parameters.Position, new Point(10, 10));
+        }
+        [TestMethod]
+        public void TestDeleteElement()
+        {
+            var SecondElement = new ElementObject(new Point(0, 0), new MyRectangle());
+            canvas.Elements.Add(TestElement);
+            canvas.Elements.Add(SecondElement);
+            canvas.Lines.Add(new Line(TestElement.Nodes[0], SecondElement.Nodes[0]));
+            Assert.AreEqual(canvas.Elements.Count, 2);
+            Assert.AreEqual(canvas.Lines.Count, 1);
+            canvas.DeleteElement(TestElement);
+            Assert.AreEqual(canvas.Elements.Count, 1);
+            Assert.AreEqual(canvas.Lines.Count, 0);
+        }
+        [TestMethod]
+        public void TestRenameElement()
+        {
+            Assert.AreEqual(TestElement.Parameters.Text, new MyRectangle().Name);
+            TestElement.Parameters.Text = "Test";
+            Assert.AreEqual(TestElement.Parameters.Text, "Test");
+        }
+        [TestMethod]
+        public void TestFileActions()
+        {
+            var SecondElement = new ElementObject(new Point(0, 0), new MyRectangle());
+            canvas.Elements.Add(TestElement);
+            canvas.Elements.Add(SecondElement);
+            canvas.Lines.Add(new Line(TestElement.Nodes[0], SecondElement.Nodes[0]));
+
+            FileActions fileActions = new FileActions(canvas);
+
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.block");
+
+            fileActions.CreateFile(path);
+
+            Assert.AreEqual(canvas.Elements.Count, 2);
+            Assert.AreEqual(canvas.Lines.Count, 1);
+
+            canvas.DeleteElement(TestElement);
+            canvas.DeleteElement(SecondElement);
+
+            Assert.AreEqual(canvas.Elements.Count, 0);
+            Assert.AreEqual(canvas.Lines.Count, 0);
+
+            fileActions.Import(path);
+
+            Assert.AreEqual(canvas.Elements.Count, 2);
+            Assert.AreEqual(canvas.Lines.Count, 1);
+
+            File.Delete(path);
+        }
+    }
+}
